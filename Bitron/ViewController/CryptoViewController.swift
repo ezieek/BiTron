1d3fbf6068232f0fc88ae403a623f33542bf4418
Ezieek <wolejko92@gmail.com>
2020-08-04 22:12:00 +0200

    Added new ViewController class

---
+++ Bitron/ViewController/CryptoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CryptoViewController.swift
+//  Bitron
+//
+//  Created by Maciej Wołejko on 04/08/2020.
+//  Copyright © 2020 Maciej Wołejko. All rights reserved.
+//
+
+import UIKit
+
+class CryptoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
