9bddb9dcf9e4625d434f0bc4d825bade401adc06
Ezieek <wolejko92@gmail.com>
2020-08-04 22:23:47 +0200

    Added podfiles

---
+++ Pods/Target Support Files/Pods-Bitron-BitronUITests/Pods-Bitron-BitronUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bitron_BitronUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Bitron_BitronUITests
+@end
---
+++ Pods/Target Support Files/Pods-Bitron/Pods-Bitron-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bitron : NSObject
+@end
+@implementation PodsDummy_Pods_Bitron
+@end
