e013f9675d2d29bddc6f2e28eef4d588f9057b79
maciejwolejko <wolejko92@gmail.com>
2020-08-05 10:12:11 +0200

    Added new files

---
+++ Bitron/Services/Networking.swift
@@ -0,0 +1,13 @@
+//
+//  Networking.swift
+//  Bitron
+//
+//  Created by Maciej Wołejko on 8/5/20.
+//  Copyright © 2020 Maciej Wołejko. All rights reserved.
+//
+
+import UIKit
+
+class Networking {
+    
+}
---
+++ Bitron/Services/Persistence.swift
@@ -0,0 +1,13 @@
+//
+//  Persistence.swift
+//  Bitron
+//
+//  Created by Maciej Wołejko on 8/5/20.
+//  Copyright © 2020 Maciej Wołejko. All rights reserved.
+//
+
+import UIKit
+
+class Persistence {
+    
+}
