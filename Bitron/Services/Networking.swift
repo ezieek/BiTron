31d3383e391d55b1dcd29e1cff9584add84025ff
maciejwolejko <wolejko92@gmail.com>
2020-08-05 10:39:12 +0200

    Added NetworkService code

--- Bitron/Services/Networking.swift
+++ Bitron/Services/Networking.swift
@@ -10,4 +10,22 @@ import UIKit
 
 class Networking {
     
+    private init() {}
+    static let shared = NetworkingService()
+    
+    func request(_ urlPath: String, completion: @escaping (Result<Data, NSError>) -> Void) {
+        
+        guard let url = URL(string: urlPath) else { return }
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, _, error) in
+            
+            if let unwrappedError = error {
+                completion(.failure(unwrappedError as NSError))
+            } else if let unwrappedData = data {
+                completion(.success(unwrappedData))
+            }
+        }
+        task.resume()
+    }
 }
