65c4c57ea260773b025b0c8995bb51f1739082a3
maciejwolejko <wolejko92@gmail.com>
2020-08-10 14:55:03 +0200

    Added label object to an DetalView class

--- Bitron/View/DetailView.swift
+++ Bitron/View/DetailView.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class DetailView: UIView {
 
+    lazy var rateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
 
+        createSubViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createSubViews() {
+        [rateLabel].forEach { addSubview($0) }
+        
+        NSLayoutConstraint.activate([
+            rateLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            rateLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
     }
 }
