fd58f9deedcb36621354c4d7dec6135f7056659d
maciejwolejko <wolejko92@gmail.com>
2020-11-05 11:32:08 +0100

    Added Charts library

--- Pods/Target Support Files/Pods-Bitron-BitronUITests/Pods-Bitron-BitronUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bitron_BitronUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Bitron_BitronUITests
-@end
--- Pods/Target Support Files/Pods-Bitron/Pods-Bitron-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bitron : NSObject
-@end
-@implementation PodsDummy_Pods_Bitron
-@end
