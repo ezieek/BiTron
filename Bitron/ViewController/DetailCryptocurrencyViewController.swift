555b0a5ecdd81797c0d903ea96a3727e5ceef24a
maciejwolejko <wolejko92@gmail.com>
2020-11-17 14:58:03 +0100

    Added function of creating CandleStickChartView

--- Bitron/ViewController/DetailCryptocurrencyViewController.swift
+++ Bitron/ViewController/DetailCryptocurrencyViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Charts
 
 class DetailCryptocurrencyViewController: UIViewController {
     
@@ -25,19 +26,22 @@ class DetailCryptocurrencyViewController: UIViewController {
     var pushedCryptocurrencyRate: String = ""
     var pushedCryptocurrencyPreviousRate: String = ""
     var pushedCryptocurrencyImage: String = ""
-    var test: Int? = 0
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //detailViewModel.detailCryptocurrencyShortName.append(pushedCryptocurrencySubName)
-        detailViewModel.detailCryptocurrencyShortName.append(pushedCryptocurrencyName)
+        detailViewModel.detailCryptocurrencyShortName.append(pushedCryptocurrencySubName)
         setupView()
         contentViewActions()
         detailViewModel.getJSON {
-            self.contentView.cryptocurrencyVolumeLabel.text = self.detailViewModel.detailCryptocurrencyVolumeValue
+            print("HIGH: \(self.detailViewModel.detailCryptocurrencyHighValue), LOW: \(self.detailViewModel.detailCryptocurrencyLowValue) VOLUME: \(self.detailViewModel.detailCryptocurrencyVolumeValue)")
+           // self.contentView.cryptocurrencyVolumeLabel.text = self.detailViewModel.detailCryptocurrencyVolumeValue
         }
+        
+        contentView.chartView.delegate = self
+        
+        setDataCount(Int(3), range: Int(10))
     }
     
     override func loadView() {
@@ -57,10 +61,37 @@ class DetailCryptocurrencyViewController: UIViewController {
 
         //add code here
     }
+    
 
+    func setDataCount(_ count: Int, range: Int) {
+        let yVals1 = (0..<count).map { (i) -> CandleChartDataEntry in
+            let high = [36155.44, 35448.5, 34400.0]
+            let low = [33650.0, 33400.0, 32431.67]
+            let open = [36104.08, 34217.79, 34358.5]
+            let close = [34161.41, 34275.86, 34275.86]
+            
+            return CandleChartDataEntry(x: Double(i), shadowH: high[i], shadowL: low[i], open: open[i], close: close[i])
+        }
+        
+        let set1 = CandleChartDataSet(entries: yVals1, label: "Data Set")
+        set1.axisDependency = .left
+        set1.setColor(UIColor(white: 80/255, alpha: 1))
+        set1.drawIconsEnabled = false
+        set1.shadowColor = .darkGray
+        set1.shadowWidth = 0.7
+        set1.decreasingColor = .red
+        set1.decreasingFilled = true
+        set1.increasingColor = UIColor(red: 122/255, green: 242/255, blue: 84/255, alpha: 1)
+        set1.increasingFilled = false
+        set1.neutralColor = .green
+        set1.valueTextColor = .white
+        
+        let data = CandleChartData(dataSet: set1)
+        contentView.chartView.data = data
+    }
     // MARK: - private
     private func setupView() {
-        navigationItem.title = "\(detailViewModel.detailCryptocurrencyShortName) (\(pushedCryptocurrencySubName))"
+        navigationItem.title = "\(pushedCryptocurrencyName) (\(pushedCryptocurrencySubName))"
         view.layer.insertSublayer(settingBackgroundColor.gradientColor, at: 0)
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "arrow-left"), style: .done, target: self, action: #selector(backButtonPressed))
     }
@@ -82,5 +113,12 @@ class DetailCryptocurrencyViewController: UIViewController {
     @objc private func deleteDataButtonPressed() {
         persistence.deleteCoreData(name: pushedCryptocurrencyName, rate: pushedCryptocurrencyRate, previousRate: pushedCryptocurrencyPreviousRate, image: pushedCryptocurrencyImage)
         coordinatorChosen?.pushBackToChosenCryptocurrencyViewController()
+    }
+}
+
+extension DetailCryptocurrencyViewController: ChartViewDelegate {
+    
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print("Entry: \(entry)")
     }
 }
