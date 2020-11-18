45d72e93409054931c795f28c6e9d738beb0b9b3
maciejwolejko <wolejko92@gmail.com>
2020-11-18 15:25:27 +0100

    Edited way of grouping data fetched from database

--- Bitron/ChosenView/ViewModel/ChosenCryptocurrencyViewModel.swift
+++ Bitron/ChosenView/ViewModel/ChosenCryptocurrencyViewModel.swift
@@ -16,36 +16,15 @@ class ChosenCryptocurrencyViewModel {
     // MARK: - Properties
     weak var timer: Timer?
     private let persistence = Persistence.shared
-    private var chosenCryptocurrencyNames: [String] = []
-    private var chosenCryptocurrencyRates: [String] = []
-    private var chosenCryptocurrencyPreviousRates: [String] = []
-    private var chosenCryptocurrencyImages: [String] = []
-    private var percentResult = 0.0
+    var chosenCryptocurrencyNames: [String] = []
+    var chosenCryptocurrencySubNames: [String] = []
+    var chosenCryptocurrencyRates: [String] = []
+    var chosenCryptocurrencyPreviousRates: [String] = []
+    var chosenCryptocurrencyImages: [String] = []
+    var percentResult = 0.0
     var percentColors: [UIColor] = []
-    var assignedCryptoNames: [String] = []
-    var assignedCryptoSubNames: [String] = []
-    var assignedCryptoIcon: [String] = []
-    var assignedCryptoRates: [String] = []
-    var assignedCryptoPreviousRates: [String] = []
     
     // MARK: - internal
-    func getCurrentValueOfSavedCryptocurrenciesFirstLoadView(completion: @escaping () -> Void) {
-        self.percentColors.removeAll()
-        let retrivedCoreData =  self.persistence.retriveCoreData()
-        
-        Alamofire.request("https://api.bitbay.net/rest/trading/ticker").responseJSON { [weak self] (response) in
-            
-            switch response.result {
-            case .success(let value):
-                self?.responseJSON(value: value, cryptocurrencyName: retrivedCoreData.name, cryptocurrencyImage: retrivedCoreData.image)
-                completion()
-            case .failure(let error):
-                print(error)
-            }
-        }
-        self.cleanAssignedCryptocurrencyData()
-    }
-    
     func getCurrentValueOfSavedCryptocurrenciesNextLoadView(completion: @escaping () -> Void) {
         let timeInterval = 1.0
         timer?.invalidate()
@@ -64,7 +43,7 @@ class ChosenCryptocurrencyViewModel {
                     print(error)
                 }
             }
-            self.cleanAssignedCryptocurrencyData()
+            self.cleanChosenCryptocurrencyData()
         })
     }
     
@@ -75,61 +54,28 @@ class ChosenCryptocurrencyViewModel {
     // MARK: - private
     private func responseJSON(value: Any, cryptocurrencyName: [String], cryptocurrencyImage: [String]) {
         let jsonValue = JSON(value)
-        var arrayCryptocurrenyNames: [String] = []
-        var arrayCryptocurrencySubNames: [String] = []
-        var arrayCryptocurrencyRates: [String] = []
-        var arrayCryptocurrencyPreviousRates: [String] = []
         
         for names in cryptocurrencyName {
             let json = JSON(jsonValue)["items"][names]
             let cryptocurrency = SelectCryptocurrency(json: json)
             let removingUselessString = names.replacingOccurrences(of: "-PLN", with: "")
-            arrayCryptocurrenyNames.append(Constants.settingMainNameOfCryptocurrency(getName: names))
-            arrayCryptocurrencySubNames.append(removingUselessString)
-            arrayCryptocurrencyRates.append(cryptocurrency.rate ?? "")
-            arrayCryptocurrencyPreviousRates.append(self.calculatingThePercentageDifference(rate: cryptocurrency.rate ?? "", previousRate: cryptocurrency.previousRate ?? ""))
-            self.assignedCryptoIcon = cryptocurrencyImage
-        }
-    
-        for i in 0..<arrayCryptocurrenyNames.count {
-            switch(arrayCryptocurrenyNames[i]) {
-            case arrayCryptocurrenyNames[i]:
-                self.assignedCryptoNames.append(arrayCryptocurrenyNames[i])
-            default:
-                print("There is a problem with the names of the fetchted cryptocurrencies!")
-            }
-
-            switch(arrayCryptocurrencySubNames[i]) {
-            case arrayCryptocurrencySubNames[i]:
-                self.assignedCryptoSubNames.append(arrayCryptocurrencySubNames[i])
-            default:
-                print("There is a problem with the subnames of the fetched cryptocurrencies!")
-            }
-    
-            switch(arrayCryptocurrencyRates[i]) {
-            case arrayCryptocurrencyRates[i]:
-                let fetchedCryptocurrencyRatesString = arrayCryptocurrencyRates[i]
-                let fetchedCryptocurrencyRatesfloatValue = Float(fetchedCryptocurrencyRatesString)
-                self.assignedCryptoRates.append(String(format: "%.2f", fetchedCryptocurrencyRatesfloatValue ?? ""))
-            default:
-                print("There is a problem with the rates of the fetched cryptocurrencies!")
-            }
-
-            switch(arrayCryptocurrencyPreviousRates[i]) {
-            case arrayCryptocurrencyPreviousRates[i]:
-                self.assignedCryptoPreviousRates.append(arrayCryptocurrencyPreviousRates[i])
-            default:
-                print("There is a problem with the previous rates of the fetched cryptocurrencies!")
-            }
+            let fetchedCryptocurrencyRatesString = cryptocurrency.rate ?? ""
+            let fetchedCryptocurrencyRatesFloatValue = Float(fetchedCryptocurrencyRatesString)
+            
+            chosenCryptocurrencyNames.append(Constants.settingMainNameOfCryptocurrency(getName: names))
+            chosenCryptocurrencySubNames.append(removingUselessString)
+            chosenCryptocurrencyRates.append(String(format: "%.2f", fetchedCryptocurrencyRatesFloatValue ?? ""))
+            chosenCryptocurrencyPreviousRates.append(self.calculatingThePercentageDifference(rate: cryptocurrency.rate ?? "", previousRate: cryptocurrency.previousRate ?? ""))
+            chosenCryptocurrencyImages.append(contentsOf: cryptocurrencyImage)
         }
     }
     
-    private func cleanAssignedCryptocurrencyData() {
-        assignedCryptoNames.removeAll()
-        assignedCryptoSubNames.removeAll()
-        assignedCryptoRates.removeAll()
-        assignedCryptoPreviousRates.removeAll()
-        assignedCryptoIcon.removeAll()
+    private func cleanChosenCryptocurrencyData() {
+        chosenCryptocurrencyNames.removeAll()
+        chosenCryptocurrencySubNames.removeAll()
+        chosenCryptocurrencyRates.removeAll()
+        chosenCryptocurrencyPreviousRates.removeAll()
+        chosenCryptocurrencyImages.removeAll()
     }
 
     private func calculatingThePercentageDifference(rate: String, previousRate: String) -> String {
