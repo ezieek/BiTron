56e5ff07d4267522a9e27635efe08514ea90fd26
maciejwolejko <wolejko92@gmail.com>
2020-11-19 16:03:13 +0100

    Added new objects

--- Bitron/DetailView/Model/DetailCryptocurrencyModel.swift
+++ Bitron/DetailView/Model/DetailCryptocurrencyModel.swift
@@ -11,20 +11,24 @@ import SwiftyJSON
 
 struct DetailCryptocurrencyModel {
     
-    var h: String? //the highest value from 24h
-    var l: String? //the lowest value from 24h
-    var v: String? //volume from 24h
+    var h: String? //the highest value
+    var l: String? //the lowest value
+    var v: String? //volume value
+    var o: String? //open value
+    var c: String? //close value
 }
 
 extension DetailCryptocurrencyModel {
     
     enum PropertyKey: String {
-        case h, l, v
+        case h, l, v, o, c
     }
     
     init(json: JSON) {
         h = json[PropertyKey.h.rawValue].stringValue
         l = json[PropertyKey.l.rawValue].stringValue
         v = json[PropertyKey.v.rawValue].stringValue
+        o = json[PropertyKey.o.rawValue].stringValue
+        c = json[PropertyKey.c.rawValue].stringValue
     }
 }
