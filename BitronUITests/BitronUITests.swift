9171b2668a615eee0c987327fa2192d7320eea7d
maciejwolejko <wolejko92@gmail.com>
2020-11-26 15:13:28 +0100

    Added test checking if selecting specified cryptocurrency does not crash application

--- BitronUITests/BitronUITests.swift
+++ BitronUITests/BitronUITests.swift
@@ -21,8 +21,7 @@ class BitronUITests: XCTestCase {
 
     func testCheckingApplicationDoesNotCrushWhenCellIsTapped() throws {
         let app = XCUIApplication()
-    
-        let names = ["Algory", "Augur", "Basic Attention Token", "Bitcoin", "Bob's Repair", "0x", "Chainlink"]
+        let names = ["0x", "Algory", "Augur", "Basic Attention Token", "Bitcoin", "Bitcoin Cash", "Golem"]
         
         while(true) {
             for i in names {
@@ -30,6 +29,20 @@ class BitronUITests: XCTestCase {
                 sleep(2)
                 app.navigationBars.buttons.element(boundBy: 0).tap()
                 sleep(3)
+            }
+        }
+    }
+    
+    func testSelectingFavoriteCryptocurrencies() throws {
+        let app = XCUIApplication()
+        let names = ["0x", "Algory", "AMLT", "Augur", "Basic Attention Token", "Bitcoin", "Bitcoin Cash", "Bitcoin Gold", "Bitcoin SV", "Blockchain Poland", "Bob\'s Repair", "Chainlink", "Dash", "Ethereum", "Experty", "Game Credits", "Golem", "Infinity Economics", "Lisk", "Lisk Machine Learning", "Litecoin", "Maker", "Neumark", "OmniseGO", "Ripple", "Stellar", "TenX", "Tron", "Zcash"]
+        
+        while(true) {
+            for i in 0..<names.count {
+                app.tabBars.buttons.element(boundBy: 1).tap()
+                sleep(1)
+                app.tables.cells.element(boundBy: i).tap()
+                sleep(2)
             }
         }
     }
