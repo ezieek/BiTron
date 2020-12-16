cd4fc0ea9926817ddad1d5cad03425dbaaad3816
maciejwolejko <wolejko92@gmail.com>
2020-12-16 15:45:33 +0100

    Edited getJSONChartData function

--- Bitron/DetailView/ViewModel/DetailCryptocurrencyViewModel.swift
+++ Bitron/DetailView/ViewModel/DetailCryptocurrencyViewModel.swift
@@ -15,30 +15,36 @@ import Charts
 class DetailCryptocurrencyViewModel {
     
     // MARK: - Properties
-    lazy var detailCryptocurrencyShortName:   String = ""
-    lazy var detailCryptocurrencyHighValue:   [String] = []
-    lazy var detailCryptocurrencyLowValue:    [String] = []
-    lazy var detailCryptocurrencyVolumeValue: [String] = []
-    lazy var detailCryptocurrencyOpenValue:   [String] = []
-    lazy var detailCryptocurrencyCloseValue:  [String] = []
-    
+    private lazy var model: [DetailCryptocurrencyModel] = [DetailCryptocurrencyModel]()
+    private lazy var high:   [String] = []
+    private lazy var low:    [String] = []
+    private lazy var volume: [String] = []
+    private lazy var open:   [String] = []
+    private lazy var close:  [String] = []
+   
     // MARK: - internal
-    func getJSONChartData(cryptocurrencyName: String, resolution: String, completion: @escaping () -> Void) {
-        AF.request("https://api.bitbay.net/rest/trading/candle/history/\(cryptocurrencyName)/\(resolution)?from=1605614400000&to=\(1605787200)000").responseJSON { (response) in
+    func getJSONChartData(cryptocurrencyName: String, resolution: String, completion: @escaping ([DetailCryptocurrencyModel]) -> ()) {
+
+        AF.request("https://api.bitbay.net/rest/trading/candle/history/\(cryptocurrencyName)/\(resolution)?from=1605614400000&to=\(1605787200)000").responseJSON { [self] (response) in
+            
             switch response.result {
+            
             case .success(let value):
-                
                 for i in 0...1 {
                     let json = JSON(value)["items"][i][1]
-                    let model = DetailCryptocurrencyModel(json: json)
-                    self.detailCryptocurrencyHighValue.append(model.h!)
-                    self.detailCryptocurrencyLowValue.append(model.l!)
-                    self.detailCryptocurrencyOpenValue.append(model.o!)
-                    self.detailCryptocurrencyCloseValue.append(model.c!)
-                    completion()
+                    self.high.append(json["h"].stringValue)
+                    self.low.append(json["l"].stringValue)
+                    self.volume.append(json["v"].stringValue)
+                    self.open.append(json["o"].stringValue)
+                    self.close.append(json["c"].stringValue)
+                    
+                    self.model.append(DetailCryptocurrencyModel(high: high[i], low: low[i], volume: volume[i], open: open[i], close: close[i]))
                 }
+                
+                completion(self.model)
+                
             case .failure(let error):
-            print(error)
+                print(error)
             }
         }
     }
