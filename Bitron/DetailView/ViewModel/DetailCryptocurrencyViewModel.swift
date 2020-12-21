0cc8d5c05223cd3106e00ca7e7337ff98ff9257c
maciejwolejko <wolejko92@gmail.com>
2020-12-21 12:58:16 +0100

    Added weak reference to avoid memory leak

--- Bitron/DetailView/ViewModel/DetailCryptocurrencyViewModel.swift
+++ Bitron/DetailView/ViewModel/DetailCryptocurrencyViewModel.swift
@@ -24,30 +24,42 @@ class DetailCryptocurrencyViewModel {
     func getJSONChartData(cryptocurrencyName: String, resolution: String, fromTimestamp: String, completion: @escaping ([DetailCryptocurrencyModel]) -> ()) {
         let currentTimestamp = Int(NSDate().timeIntervalSince1970)
 
-        AF.request("https://api.bitbay.net/rest/trading/candle/history/\(cryptocurrencyName)/\(resolution)?from=\(fromTimestamp)000&to=\(currentTimestamp)000").responseJSON { [self] (response) in
-           // let time = currentTimestamp - Int(fromTimestamp)! / 1000
-           // let numberOfSticks = time / Int(resolution)!
+        AF.request("https://api.bitbay.net/rest/trading/candle/history/\(cryptocurrencyName)/\(resolution)?from=\(fromTimestamp)000&to=\(currentTimestamp)000").responseJSON { [weak self] (response) in
 
             switch response.result {
             
             case .success(let value):
-                for i in 0...50 {//numberOfSticks {
+                for i in 0...49 {
                     let json = JSON(value)["items"][i][1]
-                    self.high.append(json["h"].stringValue)
-                    self.low.append(json["l"].stringValue)
-                    self.volume.append(json["v"].stringValue)
-                    self.open.append(json["o"].stringValue)
-                    self.close.append(json["c"].stringValue)
+                    self?.high.append(json["h"].stringValue)
+                    self?.low.append(json["l"].stringValue)
+                    self?.volume.append(json["v"].stringValue)
+                    self?.open.append(json["o"].stringValue)
+                    self?.close.append(json["c"].stringValue)
 
-                    self.model.append(DetailCryptocurrencyModel(high: high[i], low: low[i], volume: volume[i], open: open[i], close: close[i], count: 50))
+                    self?.model.append(DetailCryptocurrencyModel(
+                        high: self?.high[i] ?? "",
+                        low: self?.low[i] ?? "",
+                        volume: self?.volume[i] ?? "",
+                        open: self?.open[i] ?? "",
+                        close: self?.close[i] ?? ""))
                 }
-                
-                completion(self.model)
-                self.model.removeAll()
+
+                completion(self?.model ?? [])
                 
             case .failure(let error):
                 print(error)
             }
+            self?.cleanDetailCryptocurrencyData()
         }
+    }
+    
+    // MARK: - private
+    private func cleanDetailCryptocurrencyData() {
+        self.model.removeAll()
+        self.high.removeAll()
+        self.low.removeAll()
+        self.open.removeAll()
+        self.close.removeAll()
     }
 }
