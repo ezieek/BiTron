4fd391a5ee1222e2fe070ec8c607d809831e5dc6
Ezieek <wolejko92@gmail.com>
2021-01-12 19:56:38 +0100

    Deleted testCheckingApplicationDoesNotCrushWhenCellIsTapped() function

--- BitronUITests/BitronUITests.swift
+++ BitronUITests/BitronUITests.swift
@@ -18,32 +18,16 @@ class BitronUITests: XCTestCase {
     override func tearDownWithError() throws {
         // put code here
     }
-
-    func testCheckingApplicationDoesNotCrushWhenCellIsTapped() throws {
-        let app = XCUIApplication()
-        let tableViewCell = app.tables
-        let navBarButtonItem = app.navigationBars.buttons.element(boundBy: 0)
-        let names = ["0x", "Algory", "AMLT", "Augur", "Basic Attention Token", "Bitcoin", "Bitcoin Cash", "Bitcoin Gold", "Bitcoin SV", "Blockchain Poland", "Bob\'s Repair", "Chainlink", "Dash", "Ethereum", "Experty", "Game Credits", "Golem", "Infinity Economics", "Lisk", "Lisk Machine Learning", "Litecoin", "Maker", "Neumark", "OmniseGO", "Ripple", "Stellar", "TenX", "Tron", "Zcash"]
-        
-        while(true) {
-            for i in names {
-                tableViewCell.staticTexts[i].tap()
-                sleep(2)
-                navBarButtonItem.tap()
-                sleep(3)
-            }
-        }
-    }
     
     func testAddingFavoriteCryptocurrencies() throws {
         let app = XCUIApplication()
         let tabBarButton = app.tabBars.buttons.element(boundBy: 1)
         let tableViewCell = app.tables.cells
 
-        for i in 0..<29 {
+        for number in 0..<29 {
             tabBarButton.tap()
             sleep(1)
-            tableViewCell.element(boundBy: i).tap()
+            tableViewCell.element(boundBy: number).tap()
             sleep(2)
         }
     }
